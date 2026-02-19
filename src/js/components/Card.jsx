50e462909b8fda3ab4dded9225c902d3fa370f4b
sergio19000 <119664418+sergio19000@users.noreply.github.com>
2026-02-19 10:46:18 +0000

    arreglo home

--- src/js/components/Card.jsx
+++ src/js/components/Card.jsx
@@ -1,19 +1,49 @@
-import React from "react";
-import "./Card.css";
+import React from 'react'
+import Cardprops from './Cardprops'
 
-const Card = (props) => {
+function Card() {
   return (
-    <div className="d-flex justify-content-center mt-4 ps-4 pe-4">
-      <div className="card w-100 h-100">
-        <img src={props.image} className="card-img-top" />
-        <div className="card-body text-center">
-          <h5 className="card-title">{props.title}</h5>
-          <p className="card-text">{props.text}</p>
-          <button className="btn btn-primary">{props.buttonText}</button>
-        </div>
-      </div>
+    <div className="contenedor d-flex flex-column align-items-center">
+				
+					<div className="row justify-content-center g-1">
+						<div className="col-12 col-sm-6 col-lg-3">
+							<Cardprops
+									title="La Luz al final el túnel"
+									text="despues de tanto luchar pude hacer mi primer componente"
+									image="https://fastly.picsum.photos/id/137/300/200.jpg?hmac=7GOEmQEJNznawKi8mMjA3tPrgb6kbZZ0qCxn8H0RKFU"
+									buttonText="Find out more"
+							/>
+						</div>
+
+						<div className="col-12 col-sm-6 col-lg-3">
+							<Cardprops
+									title="Un poquito mas de luz"
+									text="También pude centrar el contenido dentro de mi card"
+									image="https://fastly.picsum.photos/id/16/2500/1667.jpg?hmac=uAkZwYc5phCRNFTrV_prJ_0rP0EdwJaZ4ctje2bY7aE"
+									buttonText="Find out more"
+							/>
+						</div>
+
+						<div className="col-12 col-sm-6 col-lg-3">
+							<Cardprops
+									title="Todo empieza a florecer"
+									text="Ahora se ve un poquito mas claro el camino"
+									image="https://fastly.picsum.photos/id/28/4928/3264.jpg?hmac=GnYF-RnBUg44PFfU5pcw_Qs0ReOyStdnZ8MtQWJqTfA"
+									buttonText="Find out more"
+							/>
+						</div>
+
+						<div className="col-12 col-sm-6 col-lg-3">
+							<Cardprops
+									title="Que bonito paisaje"
+									text="Aquí es donde quiero estar en este momento"
+									image="https://fastly.picsum.photos/id/37/2000/1333.jpg?hmac=vpYLNsQZwU2szsZc4Uo17cW786vR0GEUVq4icaKopQI"
+									buttonText="Find out more"
+							/>
+						</div>
+					</div>
     </div>
-  );
-};
+  )
+}
 
 export default Card;
--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import NavBar from "./NavBar";
 import Jumbotron from './Jumbotron';
-import Card from "./Card";
 import Footer from "./Footer";
+import Card from "./Card";
 
 function Home() {
     return (
@@ -10,48 +10,9 @@ function Home() {
             <NavBar/>
             <main className="container mt-4">
                 <Jumbotron/>
-				<div className="contenedor d-flex flex-column align-items-center">
-				
-					<div className="row justify-content-center g-1">
-						<div className="col-12 col-sm-6 col-lg-3">
-							<Card
-									title="La Luz al final el túnel"
-									text="despues de tanto luchar pude hacer mi primer componente"
-									image="https://fastly.picsum.photos/id/137/300/200.jpg?hmac=7GOEmQEJNznawKi8mMjA3tPrgb6kbZZ0qCxn8H0RKFU"
-									buttonText="Find out more"
-							/>
-						</div>
-
-						<div className="col-12 col-sm-6 col-lg-3">
-							<Card
-									title="Un poquito mas de luz"
-									text="También pude centrar el contenido dentro de mi card"
-									image="https://fastly.picsum.photos/id/16/2500/1667.jpg?hmac=uAkZwYc5phCRNFTrV_prJ_0rP0EdwJaZ4ctje2bY7aE"
-									buttonText="Find out more"
-							/>
-						</div>
-
-						<div className="col-12 col-sm-6 col-lg-3">
-							<Card
-									title="Todo empieza a florecer"
-									text="Ahora se ve un poquito mas claro el camino"
-									image="https://fastly.picsum.photos/id/28/4928/3264.jpg?hmac=GnYF-RnBUg44PFfU5pcw_Qs0ReOyStdnZ8MtQWJqTfA"
-									buttonText="Find out more"
-							/>
-						</div>
-
-						<div className="col-12 col-sm-6 col-lg-3">
-							<Card
-									title="Que bonito paisaje"
-									text="Aquí es donde quiero estar en este momento"
-									image="https://fastly.picsum.photos/id/37/2000/1333.jpg?hmac=vpYLNsQZwU2szsZc4Uo17cW786vR0GEUVq4icaKopQI"
-									buttonText="Find out more"
-							/>
-						</div>
-					</div>
-				<Footer/>
-				</div>
+				<Card/>
             </main>
+			<Footer/>
         </div>
     );
 }
